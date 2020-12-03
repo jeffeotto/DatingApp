5e9106fb93c0c4d5eb9636c35c21dda6570c366e
Jeffe Otto <jeffe.otto@gmail.com>
2020-12-03 12:26:57 +0100

    Client

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+
+
+export class NavComponent implements OnInit {
+  navbarOpen = false;
+  model: any = {}
+  
+
+  constructor(public accountService: AccountService, private router:Router,private toatstr:ToastrService) { }
+
+  ngOnInit(): void {
+    
+    
+  }
+  
+    login(){
+      this.accountService.login(this.model)
+      .subscribe(response=>{
+        this.router.navigateByUrl('/members');
+        console.log(response);
+      })
+    }
+
+    logout(){
+      this.accountService.logout();
+      this.router.navigateByUrl('/');
+    }
+    
+    toggleNavbar() {
+      this.navbarOpen = !this.navbarOpen;
+    }
+}
